e073e24116fdfe7a78f460a3066516cd6d1380bc
Aamer Hasham <aamernhasham@yahoo.ca>
2016-02-15 17:10:23 -0500

    Added about us page, not sure if it will work on Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,8 @@ Rails.application.routes.draw do
 
   get 'home/about'
 
-
-root 'home#index'
-get 'home/index'
-
+  root 'home#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

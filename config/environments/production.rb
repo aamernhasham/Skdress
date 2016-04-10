eb0c2d9c741d658e7a61f8a613d5a5a4e7b368d2
Aamer Hasham <aamernhasham@yahoo.ca>
2016-04-10 15:05:31 -0400

    Seems like the paperclip credentials issues might be fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,10 +3,9 @@ Rails.application.configure do
 
   config.paperclip_defaults={
     :storage=>:s3,
-    :s3_credentials=>
-    {
+    :s3_credentials=>{
       :bucket=>ENV['AWS_BUCKET'],
-      :access_keyIid=>ENV['AWS_ACCESS_KEY_ID'],
+      :access_key_id=>ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key=>ENV['AWS_SECRET_ACCESS_KEY']
 
     }
